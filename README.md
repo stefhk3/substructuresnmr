5dd12730ca7686e4f97a960ce54026be7249f59d
p2448410 <jacquiupfortea@gmail.com>
2021-01-04 21:33:54 +0000

    classify.py now works with combined CNN too

--- README.md
+++ README.md
@@ -6,4 +6,20 @@ pip install -r requirements.txt
 
 pip3 might also be needed depending on your system set up.
 
-Setting a virtualenviroment is recommended. 
+Setting a virtualenviroment is recommended.
+
+
+###########################################################
+###HOW TO USE CLASSIFY.PY###
+###########################################################
+
+
+If the model takes only one input then:
+```
+python3 classify.py {model_path} {image} 
+```
+
+If the model(combined CNN) takes 2 inputs:
+```
+python3 classify.py {model_path} {image_1} {image_2}
+```
--- src/classify.py
+++ src/classify.py
@@ -7,6 +7,7 @@ from keras.preprocessing.image import ImageDataGenerator
 from keras.preprocessing import image
 from keras import models
 
+combined = False
 
 def load_image(img_path, show=False):
 
@@ -29,8 +30,6 @@ def print_conclusion(prediction):
     for x in range(3):
         labeled_prediction[labels[x]] = round(prediction[0][x], 3)
 
-    print(labeled_prediction)
-
     sorted_p = collections.OrderedDict(sorted(labeled_prediction.items(), key=operator.itemgetter(1), reverse=True))
 
     # print(sorted_predictions)
@@ -38,31 +37,33 @@ def print_conclusion(prediction):
     second_item = sorted_p.popitem(last=False)
     third_item = sorted_p.popitem(last=False)
 
-
-    if (
-            (str(first_item[1])[:3] == '0.3') 
-            and 
-            (str(second_item[1])[:3] == '0.3') 
-            and 
-            (str(third_item[1])[:3] == '0.3')
-        ):
-        print("equal mixes of all")
-    elif (
-            (float(str(first_item[1])[:3]) >= 0.4) 
-            and 
-            (float(str(second_item[1])[:3]) >= 0.4)
-        ):
-        print("equal mix of " + first_item[0] + " and " + second_item[0])
+    if combined:
+        print('\n\n' + str(first_item[0]) + ' ' + 'with ' + str(first_item[1]*100) + '%% confidence')
     else:
-        print("dominant mix:  " + first_item[0])
-        if (second_item[1] > 0.2):
-            print("minor mix:  " + second_item[0])
+        if (
+                (str(first_item[1])[:3] == '0.3') 
+                and 
+                (str(second_item[1])[:3] == '0.3') 
+                and 
+                (str(third_item[1])[:3] == '0.3')
+            ):
+            print("equal mixes of all")
+        elif (
+                (float(str(first_item[1])[:3]) >= 0.4) 
+                and 
+                (float(str(second_item[1])[:3]) >= 0.4)
+            ):
+            print("equal mix of " + first_item[0] + " and " + second_item[0])
         else:
-            print("negligent mix: " + second_item[0])
-        if (third_item[1] < 0.2):
-            print("negligent mix:  " + third_item[0])
-        else:
-            print("minor mix:  " + third_item[0])
+            print("dominant mix:  " + first_item[0])
+            if (second_item[1] > 0.2):
+                print("minor mix:  " + second_item[0])
+            else:
+                print("negligent mix: " + second_item[0])
+            if (third_item[1] < 0.2):
+                print("negligent mix:  " + third_item[0])
+            else:
+                print("minor mix:  " + third_item[0])
 
 def test_the_model():
     test_path = '../data/hsqc/test/'
@@ -77,11 +78,26 @@ def test_the_model():
                     print_conclusion(network.predict(loaded_img))
         print('\n\n\n')
 
+def model_classification(network, imgs):
+    prediction = network.predict(imgs)
+    print_conclusion(prediction)
+
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description='Process some integers.')
-    parser.add_argument('filepath', help='path of the model with h5 extension')
+    parser = argparse.ArgumentParser(description='Load and run an h5 model.')
+    parser.add_argument('modelpath', help='path of the model with h5 extension')
+    parser.add_argument('imgpaths', nargs="+", help='path of the images')
     args = parser.parse_args()
 
-    network = models.load_model(args.filepath)
+    imgs = []
+
+    for path in args.imgpaths:
+        img = load_image(path)
+        imgs.append(img)
     
-    test_the_model()
+    network = models.load_model(args.modelpath)
+
+    if len(imgs) == 1:
+        model_classification(network, imgs[0])
+    else:
+        combined = True
+        model_classification(network, imgs)
